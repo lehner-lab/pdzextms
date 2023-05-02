5be632e10787f46a618aff0b28dcacb14311d68e
andrefaure <a.jfaure@gmail.com>
2023-05-02 15:13:37 +0200

    add output_dir option

--- R/pdzextms.R
+++ R/pdzextms.R
@@ -5,14 +5,16 @@
 #'
 #' @param startStage Start at a specified analysis stage (default:1)
 #' @param stopStage Stop at a specified analysis stage (default:15)
-#' @param base_dir Base directory for all output files (default:NB private CRG server path; change accordingly)
+#' @param base_dir Base directory for all input files (default:NB private CRG server path; change accordingly)
+#' @param output_dir Output directory for all output files (default:same as base_dir)
 #'
 #' @return Nothing
 #' @export
 pdzextms <- function(
   startStage = 1,
   stopStage = 6,
-  base_dir = "/users/project/prj004631/afaure/DMS/Results/pdzextms_proj"
+  base_dir = "/users/project/prj004631/afaure/DMS/Results/pdzextms_proj",
+  output_dir = ""
   ){
 
   # startStage=1
@@ -57,6 +59,11 @@ pdzextms <- function(
   first_stage <- startStage
   last_stage <- stopStage
 
+  #Default output directory
+  if(output_dir==""){
+    output_dir <- base_dir
+  }
+
   ### Fit thermo models
   ###########################
 
@@ -74,7 +81,7 @@ pdzextms <- function(
     mochi_outpath = file.path(base_dir, "Data", "mochi", "PSD95-PDZ3"),
     literature_free_energies = file.path(base_dir, "Data", "in_vitro", "PDZ_literature_free_energies.txt"),
     position_offset = 311,
-    outpath = pdzextms__format_dir(dir_suffix="_pdzextms_thermo_model_results_PSD95-PDZ3", stagenum=stagenum, base_dir=base_dir),
+    outpath = pdzextms__format_dir(dir_suffix="_pdzextms_thermo_model_results_PSD95-PDZ3", stagenum=stagenum, base_dir=output_dir),
     colour_scheme = colour_scheme,
     execute = (first_stage <= stagenum & last_stage >= stagenum))
   #PSD95-PDZ3 Nterm extension
@@ -82,7 +89,7 @@ pdzextms <- function(
     mochi_outpath = file.path(base_dir, "Data", "mochi", "PSD95-PDZ3_N"),
     literature_free_energies = file.path(base_dir, "Data", "in_vitro", "PDZ_literature_free_energies.txt"),
     position_offset = 311,
-    outpath = pdzextms__format_dir(dir_suffix="_pdzextms_thermo_model_results_PSD95-PDZ3_N", stagenum=stagenum, base_dir=base_dir),
+    outpath = pdzextms__format_dir(dir_suffix="_pdzextms_thermo_model_results_PSD95-PDZ3_N", stagenum=stagenum, base_dir=output_dir),
     colour_scheme = colour_scheme,
     execute = (first_stage <= stagenum & last_stage >= stagenum))
   #PSD95-PDZ3 Cterm extension
@@ -90,7 +97,7 @@ pdzextms <- function(
     mochi_outpath = file.path(base_dir, "Data", "mochi", "PSD95-PDZ3_C"),
     literature_free_energies = file.path(base_dir, "Data", "in_vitro", "PDZ_literature_free_energies.txt"),
     position_offset = 311,
-    outpath = pdzextms__format_dir(dir_suffix="_pdzextms_thermo_model_results_PSD95-PDZ3_C", stagenum=stagenum, base_dir=base_dir),
+    outpath = pdzextms__format_dir(dir_suffix="_pdzextms_thermo_model_results_PSD95-PDZ3_C", stagenum=stagenum, base_dir=output_dir),
     colour_scheme = colour_scheme,
     execute = (first_stage <= stagenum & last_stage >= stagenum))
   #PSD95-PDZ3 N and Cterm extension
@@ -98,7 +105,7 @@ pdzextms <- function(
     mochi_outpath = file.path(base_dir, "Data", "mochi", "PSD95-PDZ3_NC"),
     literature_free_energies = file.path(base_dir, "Data", "in_vitro", "PDZ_literature_free_energies.txt"),
     position_offset = 311,
-    outpath = pdzextms__format_dir(dir_suffix="_pdzextms_thermo_model_results_PSD95-PDZ3_NC", stagenum=stagenum, base_dir=base_dir),
+    outpath = pdzextms__format_dir(dir_suffix="_pdzextms_thermo_model_results_PSD95-PDZ3_NC", stagenum=stagenum, base_dir=output_dir),
     colour_scheme = colour_scheme,
     execute = (first_stage <= stagenum & last_stage >= stagenum))
 
@@ -109,25 +116,25 @@ pdzextms <- function(
   #PSD95-PDZ3 only
   pdzextms_structure_metrics(
     input_file = file.path(base_dir, paste0("001", "_pdzextms_thermo_model_results_PSD95-PDZ3"), "dg_singles.txt"),
-    outpath = pdzextms__format_dir(dir_suffix="_pdzextms_structure_metrics_PSD95-PDZ3", stagenum=stagenum, base_dir=base_dir),
+    outpath = pdzextms__format_dir(dir_suffix="_pdzextms_structure_metrics_PSD95-PDZ3", stagenum=stagenum, base_dir=output_dir),
     pdb_file = file.path(base_dir, "Data", "pdb", "1be9.pdb"),
     execute = (first_stage <= stagenum & last_stage >= stagenum))
   #PSD95-PDZ3 Nterm extension
   pdzextms_structure_metrics(
     input_file = file.path(base_dir, paste0("001", "_pdzextms_thermo_model_results_PSD95-PDZ3_N"), "dg_singles.txt"),
-    outpath = pdzextms__format_dir(dir_suffix="_pdzextms_structure_metrics_PSD95-PDZ3_N", stagenum=stagenum, base_dir=base_dir),
+    outpath = pdzextms__format_dir(dir_suffix="_pdzextms_structure_metrics_PSD95-PDZ3_N", stagenum=stagenum, base_dir=output_dir),
     pdb_file = file.path(base_dir, "Data", "pdb", "1be9.pdb"),
     execute = (first_stage <= stagenum & last_stage >= stagenum))
   #PSD95-PDZ3 Cterm extension
   pdzextms_structure_metrics(
     input_file = file.path(base_dir, paste0("001", "_pdzextms_thermo_model_results_PSD95-PDZ3_C"), "dg_singles.txt"),
-    outpath = pdzextms__format_dir(dir_suffix="_pdzextms_structure_metrics_PSD95-PDZ3_C", stagenum=stagenum, base_dir=base_dir),
+    outpath = pdzextms__format_dir(dir_suffix="_pdzextms_structure_metrics_PSD95-PDZ3_C", stagenum=stagenum, base_dir=output_dir),
     pdb_file = file.path(base_dir, "Data", "pdb", "1be9.pdb"),
     execute = (first_stage <= stagenum & last_stage >= stagenum))
   #PSD95-PDZ3 N and Cterm extension
   pdzextms_structure_metrics(
     input_file = file.path(base_dir, paste0("001", "_pdzextms_thermo_model_results_PSD95-PDZ3_NC"), "dg_singles.txt"),
-    outpath = pdzextms__format_dir(dir_suffix="_pdzextms_structure_metrics_PSD95-PDZ3_NC", stagenum=stagenum, base_dir=base_dir),
+    outpath = pdzextms__format_dir(dir_suffix="_pdzextms_structure_metrics_PSD95-PDZ3_NC", stagenum=stagenum, base_dir=output_dir),
     pdb_file = file.path(base_dir, "Data", "pdb", "1be9.pdb"),
     execute = (first_stage <= stagenum & last_stage >= stagenum))
 
@@ -146,7 +153,7 @@ pdzextms <- function(
   #     "PSD95-PDZ3" = file.path(base_dir, paste0("002", "_pdzextms_structure_metrics_PSD95-PDZ3"), "dg_singles.txt"),
   #     "GRB2-SH3" = file.path(base_dir, paste0("002", "_pdzextms_structure_metrics_GRB2-SH3"), "dg_singles.txt")),
   #   val_inpath = file.path(base_dir, "Data", "experimental_validations", "GRB2-SH3", "experimenal_validations_ODs.txt"),
-  #   outpath = pdzextms__format_dir(dir_suffix="_pdzextms_fitness_plots", stagenum=stagenum, base_dir=base_dir),
+  #   outpath = pdzextms__format_dir(dir_suffix="_pdzextms_fitness_plots", stagenum=stagenum, base_dir=output_dir),
   #   colour_scheme = colour_scheme,
   #   execute = (first_stage <= stagenum & last_stage >= stagenum))
   
@@ -159,7 +166,7 @@ pdzextms <- function(
   #   input_file = file.path(base_dir, paste0("002", "_pdzextms_structure_metrics_GB1"), "dg_singles.txt"),
   #   input_file_fitness = file.path(base_dir, "Data", "fitness", "GB1"),
   #   domain_name = "GB1",
-  #   outpath = pdzextms__format_dir(dir_suffix="_pdzextms_fitness_heatmaps_GB1", stagenum=stagenum, base_dir=base_dir),
+  #   outpath = pdzextms__format_dir(dir_suffix="_pdzextms_fitness_heatmaps_GB1", stagenum=stagenum, base_dir=output_dir),
   #   colour_scheme = colour_scheme,
   #   plot_width = 11,
   #   plot_traits = "Binding",
@@ -170,7 +177,7 @@ pdzextms <- function(
   #   input_file_fitness = file.path(base_dir, "Data", "fitness", "PSD95-PDZ3"),
   #   input_file_MSA = file.path(base_dir, "Data", "MSA", "PSD95-PDZ3", "frequencies.csv"),
   #   domain_name = "PSD95 PDZ3",
-  #   outpath = pdzextms__format_dir(dir_suffix="_pdzextms_fitness_heatmaps_PSD95-PDZ3", stagenum=stagenum, base_dir=base_dir),
+  #   outpath = pdzextms__format_dir(dir_suffix="_pdzextms_fitness_heatmaps_PSD95-PDZ3", stagenum=stagenum, base_dir=output_dir),
   #   colour_scheme = colour_scheme,
   #   plot_width = 15,
   #   execute = (first_stage <= stagenum & last_stage >= stagenum))
@@ -180,7 +187,7 @@ pdzextms <- function(
   #   input_file_fitness = file.path(base_dir, "Data", "fitness", "GRB2-SH3"),
   #   input_file_MSA = file.path(base_dir, "Data", "MSA", "GRB2-SH3", "frequencies.csv"),
   #   domain_name = "GRB2 SH3",
-  #   outpath = pdzextms__format_dir(dir_suffix="_pdzextms_fitness_heatmaps_GRB2-SH3", stagenum=stagenum, base_dir=base_dir),
+  #   outpath = pdzextms__format_dir(dir_suffix="_pdzextms_fitness_heatmaps_GRB2-SH3", stagenum=stagenum, base_dir=output_dir),
   #   colour_scheme = colour_scheme,
   #   plot_width = 11,
   #   execute = (first_stage <= stagenum & last_stage >= stagenum))
@@ -200,7 +207,7 @@ pdzextms <- function(
   #     "PSD95-PDZ3" = file.path(base_dir, "Data", "MSA", "PSD95-PDZ3", "frequencies.csv"),
   #     "GRB2-SH3" = file.path(base_dir, "Data", "MSA", "GRB2-SH3", "frequencies.csv")
   #   ),
-  #   outpath = pdzextms__format_dir(dir_suffix="_pdzextms_free_energy_scatterplots", stagenum=stagenum, base_dir=base_dir),
+  #   outpath = pdzextms__format_dir(dir_suffix="_pdzextms_free_energy_scatterplots", stagenum=stagenum, base_dir=output_dir),
   #   colour_scheme = colour_scheme,
   #   execute = (first_stage <= stagenum & last_stage >= stagenum))
 
@@ -212,7 +219,7 @@ pdzextms <- function(
   # pdzextms_free_energy_heatmaps(
   #   input_file = file.path(base_dir, paste0("002", "_pdzextms_structure_metrics_GB1"), "dg_singles.txt"),
   #   domain_name = "GB1",
-  #   outpath = pdzextms__format_dir(dir_suffix="_pdzextms_free_energy_heatmaps_GB1", stagenum=stagenum, base_dir=base_dir),
+  #   outpath = pdzextms__format_dir(dir_suffix="_pdzextms_free_energy_heatmaps_GB1", stagenum=stagenum, base_dir=output_dir),
   #   colour_scheme = colour_scheme,
   #   plot_width = 11,
   #   execute = (first_stage <= stagenum & last_stage >= stagenum))
@@ -220,7 +227,7 @@ pdzextms <- function(
   # pdzextms_free_energy_heatmaps(
   #   input_file = file.path(base_dir, paste0("002", "_pdzextms_structure_metrics_PSD95-PDZ3"), "dg_singles.txt"),
   #   domain_name = "PSD95 PDZ3",
-  #   outpath = pdzextms__format_dir(dir_suffix="_pdzextms_free_energy_heatmaps_PSD95-PDZ3", stagenum=stagenum, base_dir=base_dir),
+  #   outpath = pdzextms__format_dir(dir_suffix="_pdzextms_free_energy_heatmaps_PSD95-PDZ3", stagenum=stagenum, base_dir=output_dir),
   #   colour_scheme = colour_scheme,
   #   plot_width = 15,
   #   execute = (first_stage <= stagenum & last_stage >= stagenum))
@@ -228,7 +235,7 @@ pdzextms <- function(
   # pdzextms_free_energy_heatmaps(
   #   input_file = file.path(base_dir, paste0("002", "_pdzextms_structure_metrics_GRB2-SH3"), "dg_singles.txt"),
   #   domain_name = "GRB2 SH3",
-  #   outpath = pdzextms__format_dir(dir_suffix="_pdzextms_free_energy_heatmaps_GRB2-SH3", stagenum=stagenum, base_dir=base_dir),
+  #   outpath = pdzextms__format_dir(dir_suffix="_pdzextms_free_energy_heatmaps_GRB2-SH3", stagenum=stagenum, base_dir=output_dir),
   #   colour_scheme = colour_scheme,
   #   plot_width = 11,
   #   execute = (first_stage <= stagenum & last_stage >= stagenum))
